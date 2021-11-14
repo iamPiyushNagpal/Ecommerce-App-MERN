95ef43793cd6cf00bf2d100e981ff7103595f5bb
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-11-14 15:44:14 +0530

    Move product routes in separate file

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,12 @@
 import express from 'express';
-import products from './data/products.js';
 import './config/env.js';
 import './database/db.js';
-const app = express();
+import productRoutes from './routes/productRoutes.js';
+
 const port = process.env.PORT;
 
-app.get('/api/products', (req, res) => {
-    res.send(products);
-})
+const app = express();
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.send(product);
-})
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server started spinning on port ${port}`));
---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import productModel from '../models/productModel.js';
+import asyncHandler from "express-async-handler";
+
+const router = express.Router();
+
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await productModel.find({});
+    res.send(products);
+}))
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await productModel.findById(req.params.id);
+    if (product)
+        res.send(product);
+    else {
+        res.status(404).send({ message: `Product not found` })
+    }
+}))
+
+export default router;
