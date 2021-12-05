ea4cfd722d8a9411faf1640d1e7a11d679871af2
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-05 12:35:38 +0530

    Create order controller and route

---
+++ backend/controllers/orderControllers.js
@@ -0,0 +1,29 @@
+import orderModel from '../models/orderModel.js';
+import asyncHandler from "express-async-handler";
+
+const addOrderItems = asyncHandler(async (req, res) => {
+    const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice,
+        shippingPrice, totalPrice } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order Items');
+    }
+    else {
+        const order = new orderModel({
+            orderItems,
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice
+        })
+
+        const createdOrder = await order.save();
+        res.status(201).send(createdOrder);
+    }
+})
+
+export { addOrderItems };
