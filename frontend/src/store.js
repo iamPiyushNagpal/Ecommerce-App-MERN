c0e30e477cdafd32d4da3ce7073806fd659cda99
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-07 14:52:41 +0530

    Add orderPaid Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,9 @@ import { cartReducer } from './reducers/cartReducers';
 import {
     userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer
 } from './reducers/userReducers';
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers';
+import {
+    orderCreateReducer, orderDetailsReducer, orderPaidReducer
+} from './reducers/orderReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -17,7 +19,8 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
-    orderDetails: orderDetailsReducer
+    orderDetails: orderDetailsReducer,
+    orderPaid: orderPaidReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
