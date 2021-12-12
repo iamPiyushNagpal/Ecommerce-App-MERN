a6f7bdbfa454a0bce2d16ceebab078eff5d04779
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-12 14:27:07 +0530

    Create Product Review Endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express';
 const router = express.Router();
 import {
-    getProducts, getProductById, deleteProduct, updateProduct, createProduct
+    getProducts, getProductById, deleteProduct, updateProduct, createProduct,
+    createProductReview
 } from '../controllers/productControllers.js';
 import { auth, adminAuth } from '../middleware/authMiddleware.js';
 
 router.route('/')
     .get(getProducts)
     .post(auth, adminAuth, createProduct);
+
+router.route('/:id/reviews').post(auth, createProductReview);
 
 router.route('/:id')
     .get(getProductById)
