13087808815e6e48e7189b8d5d5793c1839bcebf
iamPiyushNagpal <iampiyushnagpal@gmail.com>
2021-12-12 16:21:38 +0530

    Add create product review Action & Reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -4,7 +4,9 @@ import {
     PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL,
     PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL,
     PRODUCT_CREATE_RESET, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS,
-    PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_RESET
+    PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_RESET, PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAIL,
+    PRODUCT_CREATE_REVIEW_RESET
 } from '../constants/productConstants';
 
 const productListReducer = (state = { products: [] }, action) => {
@@ -76,7 +78,22 @@ const productUpdateReducer = (state = { product: {} }, action) => {
     }
 }
 
+const productReviewCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCT_CREATE_REVIEW_REQUEST:
+            return { loading: true }
+        case PRODUCT_CREATE_REVIEW_SUCCESS:
+            return { loading: false, success: true }
+        case PRODUCT_CREATE_REVIEW_FAIL:
+            return { loading: false, error: action.payload }
+        case PRODUCT_CREATE_REVIEW_RESET:
+            return {};
+        default:
+            return state
+    }
+}
+
 export {
     productListReducer, productDetailsReducer, productDeleteReducer,
-    productCreateReducer, productUpdateReducer
+    productCreateReducer, productUpdateReducer, productReviewCreateReducer
 };
